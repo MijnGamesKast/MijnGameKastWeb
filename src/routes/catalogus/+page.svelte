048d6a340589e6c34a44d524177f9448572197c7
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-04-09 22:41:18 +0200

    catalogus

---
+++ src/routes/catalogus/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Catalogus pagina</h1>
+<p>Op deze pagina komen alle games te staan van de catalogus</p>
+
+<a href="/catalogus/1">Catalogus item 1</a><br>
+<a href="/catalogus/2">Catalogus item 2</a><br>
+<a href="/catalogus/3">Catalogus item 3</a><br>
