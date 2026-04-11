210817bb87e06865767eda0fb013a5d10d6b9260
Jeroen <s1215501@student.windesheim.nl>
2026-04-11 22:17:08 +0200

    Revert "Feature/routing setup"

--- src/routes/catalogus/[id]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	let { data } = $props();
-</script>
-
-<h2>Game detail</h2>
-<p>ID: {data.gameId}</p>
-
-<a href="/catalogus/{data.gameId}/toevoegen-aan-collectie">Toevoegen aan collectie</a>
