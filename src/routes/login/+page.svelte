2d21948f73c3e2dcddc63ee50a68d67d2b848240
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-16 18:46:50 +0200

    auth: added login/register styles and updated Svelte template structure

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,45 +6,49 @@
 
 <h1>Inloggen</h1>
 
-{#if form?.message}
-	<p style="color: red; font-weight: bold;">{form.message}</p>
-{/if}
+<div id="loginRegisterWrapper">
+	{#if form?.message}
+		<p style="color: red; font-weight: bold;">{form.message}</p>
+	{/if}
 
-<form method="POST">
-	<div class="formGroup" style="margin-bottom: 1rem;">
-		<label for="identifier">Gebruikersnaam of e-mailadres</label><br />
-		<input
-			id="identifier"
-			name="identifier"
-			type="text"
-			value={form?.values?.identifier ?? ''}
-		/>
+	<form method="POST">
+		<div class="formGroup" style="margin-bottom: 1rem;">
+			<label for="identifier">Gebruikersnaam of e-mailadres</label><br />
+			<input
+				id="identifier"
+				name="identifier"
+				type="text"
+				value={form?.values?.identifier ?? ''}
+			/>
 
-		{#if form?.fieldErrors?.identifier?.length}
-			<ul style="color: red; margin-top: 0.5rem;">
-				{#each form.fieldErrors.identifier as error}
-					<li>{error}</li>
-				{/each}
-			</ul>
-		{/if}
-	</div>
+			{#if form?.fieldErrors?.identifier?.length}
+				<ul style="color: red; margin-top: 0.5rem;">
+					{#each form.fieldErrors.identifier as error}
+						<li>{error}</li>
+					{/each}
+				</ul>
+			{/if}
+		</div>
 
-	<div class="formGroup" style="margin-bottom: 1rem;">
-		<label for="password">Wachtwoord</label><br />
-		<input
-			id="password"
-			name="password"
-			type="password"
-		/>
+		<div class="formGroup" style="margin-bottom: 1rem;">
+			<label for="password">Wachtwoord</label><br />
+			<input
+				id="password"
+				name="password"
+				type="password"
+			/>
 
-		{#if form?.fieldErrors?.password?.length}
-			<ul style="color: red; margin-top: 0.5rem;">
-				{#each form.fieldErrors.password as error}
-					<li>{error}</li>
-				{/each}
-			</ul>
-		{/if}
-	</div>
+			{#if form?.fieldErrors?.password?.length}
+				<ul style="color: red; margin-top: 0.5rem;">
+					{#each form.fieldErrors.password as error}
+						<li>{error}</li>
+					{/each}
+				</ul>
+			{/if}
+		</div>
 
-	<button type="submit">Inloggen</button>
-</form>
+		<p>Nog geen account? Registreer je <a href="/registreren">hier.</a></p>
+
+		<button type="submit">Inloggen</button>
+	</form>
+</div>
--- src/routes/registreren/+page.svelte
+++ src/routes/registreren/+page.svelte
@@ -1,1 +1,31 @@
-<h2>Registeren</h2>
+<script>
+</script>
+
+<h1>Registreren</h1>
+
+<div id="loginRegisterWrapper">
+	<form method="POST">
+		<div class="formGroup">
+			<label for="username">Gebruikersnaam *</label><br />
+			<input id="username" name="username" type="text" required/>
+		</div>
+
+		<div class="formGroup">
+			<label for="email">E-mailadres *</label><br />
+			<input id="email" name="email" type="email" required/>
+		</div>
+
+		<div class="formGroup">
+			<label for="password">Wachtwoord *</label><br />
+			<input id="password" name="password" type="password" required/>
+		</div>
+
+		<div class="formGroup">
+			<label for="passwordRepeat">Wachtwoord herhalen *</label><br />
+			<input id="passwordRepeat" name="passwordRepeat" type="password" required/>
+		</div>
+		<p>Al een account? login via <a href="/registreren">hier.</a></p>
+
+		<button type="submit">Registreren</button>
+	</form>
+</div>
