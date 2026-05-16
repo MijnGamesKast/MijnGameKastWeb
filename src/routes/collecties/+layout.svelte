a0dfe43a4d65aecbb0444dd85bb646f55216885b
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-16 22:32:27 +0200

    collecties: added "My Collections" and "Collection Details" pages, updated layout, and introduced collection styles

---
+++ src/routes/collecties/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div id="CollectionWrapper">
+	<div class="collections card">
+		<h2>Collecties</h2>
+		<a href="/collecties/5" class="collection">
+			<span class="collectieNaam">Alle games</span> <span class="collectieEigenaar">Openbaar</span>
+		</a>
+		<a href="/collecties/6" class="collection">
+			<span class="collectieNaam">Backlog</span> <span class="collectieEigenaar">Privé</span>
+		</a>
+		<a href="/collecties/7" class="collection">
+			<span class="collectieNaam">Favorieten</span> <span class="collectieEigenaar">Privé</span>
+		</a>
+	</div>
+
+
+	<div class="collectionGames card">
+		{@render children()}
+	</div>
+</div>
+
+<!--<div class="collections-public-wrapper">-->
+<!--	<div class="page-header">-->
+<!--		<h1>Openbare collecties</h1>-->
+<!--		<p>Ontdek collecties van andere gamers.</p>-->
+<!--	</div>-->
+<!--	-->
+<!--</div>-->
--- src/routes/collecties/[collectie]/+page.svelte
+++ src/routes/collecties/[collectie]/+page.svelte
@@ -1,31 +1,15 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-
-	let { data }: { data: PageData } = $props();
+<script>
+import logo from '$lib/images/GameCoverPlaceholder.jpg';
 </script>
 
-<a href="/collecties">Terug naar collecties</a>
-
-{#if data.collectie}
-	<h1>{data.collectie.name}</h1>
-	<p>{data.collectie.description}</p>
-{:else}
-	<h1>Collectie</h1>
-{/if}
-
-{#if data.message}
-	<p style="color: red; font-weight: bold;">{data.message}</p>
-{/if}
-
-{#if data.games.length === 0}
-	<p>Er zitten nog geen games in deze collectie.</p>
-{:else}
-	<ul style="list-style: none; padding: 0;">
-		{#each data.games as game}
-			<li style="margin-bottom: 1rem; border: 1px solid #ccc; padding: 1rem;">
-				<h2>{game.title}</h2>
-				<p>{game.description}</p>
-			</li>
-		{/each}
-	</ul>
-{/if}
+<h2>Collectienaam</h2>
+<div class="collectionGame">
+	<div class="gameInformation">
+		<img src={logo} alt="Een placeholder image voor gamecover">
+		<div class="collectionNameDescription">
+			<h3 class="gameTitle">Game</h3>
+			<p class="gameDescription">Een verkorte versie van het hele ver...</p>
+		</div>
+	</div>
+	<a href="/catalogus/1">Bekijk details</a>
+</div>
