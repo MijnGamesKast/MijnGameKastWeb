64c4e5d6582f1d8b64610bcf65fbb5d2edd6c75f
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-16 17:32:24 +0200

    catalogus: redesigned "Add Game to Collection" page with new card-based layout, added platform and genre styles, and updated CSS structure

--- src/routes/catalogus/[id]/toevoegen-aan-collectie/+page.svelte
+++ src/routes/catalogus/[id]/toevoegen-aan-collectie/+page.svelte
@@ -2,7 +2,45 @@
 	let { data } = $props();
 </script>
 
-<a href="/catalogus/{data.gameId}">Terug naar game</a>
+<div id="AddGameToCollectionWrapper">
+	<div class="gameInformation card">
+		<h2>Game</h2>
+		<p>Beschrijvijng van de game</p>
+		<div class="platforms">
+			<p class="platform box">Platform 1</p>
+			<p class="platform box">Platform 2</p>
+			<p class="platform box">Platform 3</p>
+			<p class="platform box">Platform 4</p>
+		</div>
 
-<h2>Toevoegen aan collectie</h2>
-<p>Game ID: {data.gameId}</p>
+		<div class="genres">
+			<p class="genre box">Genre 1</p>
+			<p class="genre box">Genre 2</p>
+			<p class="genre box">Genre 3</p>
+			<p class="genre box">Genre 4</p>
+		</div>
+		<a href="/catalogus/{data.gameId}" class="box">Bekijk details</a>
+	</div>
+
+	<div class="chooseCollections card">
+		<h2>Kies collectie</h2>
+		<form action="#" method="get" class="collections">
+			<div class="collection">
+				<input type="checkbox" id="collection1" name="collectionId[]" value="1">
+				<label for="collection1">Collectienaam</label>
+			</div>
+			<div class="collection">
+				<input type="checkbox" id="collection2" name="collectionId[]" value="2">
+				<label for="collection2">Collectienaam</label>
+			</div>
+			<div class="collection">
+				<input type="checkbox" id="collection3" name="collectionId[]" value="3">
+				<label for="collection3">Collectienaam</label>
+			</div>
+			<div class="collectionButtons">
+				<a href="/catalogus/{data.gameId}">Annuleren</a>
+				<button type="submit">Toevoegen</button>
+			</div>
+		</form>
+	</div>
+</div>
