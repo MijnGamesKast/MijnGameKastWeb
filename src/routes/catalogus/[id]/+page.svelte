00affcb603287e86286698bcde1ea52ea7722220
jeroenWindesheim <s1215501@student.windesheim.nl>
2026-05-17 18:03:21 +0200

    catalogus: updated game detail page to dynamically render platforms and genres; refactored props usage and added optional type definitions

--- src/routes/catalogus/[id]/+page.svelte
+++ src/routes/catalogus/[id]/+page.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
 	import logo from '$lib/images/GameCoverPlaceholder.jpg';
 
-	type Game = {
-		id: number;
-		title: string;
-		description: string;
-	};
-
-	type Gebruiker = {
-		userId: number | null;
-		username: string | null;
-		email: string | null;
-		role: number | null;
-		expiresAt: string | null;
-	} | null;
-
-	let { data }: { data: { game: Game; gebruiker: Gebruiker } } = $props();
+	// let { data }: { data: { game: Game; gebruiker: Gebruiker } } = $props();
+	let { data } = $props();
 
 	const isModerator = data.gebruiker?.role === 1;
 </script>
@@ -47,21 +34,28 @@
 
 		<div class="catalogGameGroup">
 			<h2>Platform(en)</h2>
-<!--			<p>Playstation 3, Playstation 4, Playstation 5, Xbox 360, Xbox One, Xbox Series S/X, PC</p>-->
-			<ul>
-				<li>Playstation 3</li>
-				<li>Playstation 4</li>
-				<li>Playstation 5</li>
-			</ul>
+			{#if data.game.platforms.length > 0}
+				<ul>
+					{#each data.game.platforms as platform}
+						<li>{platform.platformName}</li>
+					{/each}
+				</ul>
+			{:else}
+				<p>Geen platformen toegevoegd</p>
+			{/if}
 		</div>
 
 		<div class="catalogGameGroup">
 			<h2>Genre(s)</h2>
-			<ul>
-				<li>Shooter</li>
-				<li>Action</li>
-				<li>Simulator</li>
-			</ul>
+			{#if data.game.genres.length > 0}
+				<ul>
+					{#each data.game.genres as genre}
+						<li>{genre.genreName}</li>
+					{/each}
+				</ul>
+			{:else}
+				<p>Geen genres toegevoegd</p>
+			{/if}
 		</div>
 
 
